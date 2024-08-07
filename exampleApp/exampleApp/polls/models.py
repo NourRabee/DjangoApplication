19630d4d81f155814cad8cc8df9c76e9e83d17d1
Nour Saleh Rabee <rabee.nour123@gmail.com>
2024-08-07 09:47:46 +0300

    Django Serializer

---
+++ exampleApp/exampleApp/polls/models.py
@@ -0,0 +1,52 @@
+from _ast import Store
+
+from django.db import models
+
+
+class Category(models.Model):
+    # Primary keys (IDs) are added automatically.
+    # Reference: https://docs.djangoproject.com/en/5.0/intro/tutorial02/
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    model_year = models.PositiveIntegerField()
+    list_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class StoreProduct(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.store} - {self.product} - {self.quantity}'
