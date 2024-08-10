a9ee663307b519e5843d0fc1b847489c37182664
Nour Saleh Rabee <rabee.nour123@gmail.com>
2024-08-10 15:01:45 +0300

    Get products with store details and category info

--- exampleApp/exampleApp/polls/urls.py
+++ exampleApp/exampleApp/polls/urls.py
@@ -6,21 +6,25 @@ from . import views
 urlpatterns = [
     # The ability to add a products to a certain brand
     # post api/brands/{brand_id}/products
-    path('brands/<int:brand_id>/products', views.add_product_to_brand),
+    path('brands/<int:brand_id>/products/', views.add_product_to_brand),
 
     # Get all the categories that exist for certain store
     # GET stores/{store_id}/products/categories
-    path('stores/<int:store_id>/products/categories', views.get_categories_of_certain_store),
+    path('stores/<int:store_id>/products/categories/', views.get_categories_of_certain_store),
 
     # Get a list of the all the categories in the website
     # Get api/categories
-    path('categories', views.get_categories),
+    path('categories/', views.get_categories),
 
     # Get all products details for a certain store
     # Get api/stores/{store_id}/products
-    path('stores/<int:store_id>/products', views.get_products_of_certain_store),
+    path('stores/<int:store_id>/products/', views.get_products_of_certain_store),
 
     # Delete a product for a certain store
     # Delete api/stores/{store_id}/products/{product_id}
     path('stores/<int:store_id>/products/<int:product_id>', views.delete_product_from_store),
+
+    # Get Products with Store Details and Category Information
+    # Get api/products
+    path('products/', views.get_products_details),
 ] + debug_toolbar_urls()
--- exampleApp/exampleApp/polls/views.py
+++ exampleApp/exampleApp/polls/views.py
@@ -32,6 +32,36 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name']
 
 
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Store
+        fields = ['id', 'name', 'phone', 'email', 'street', 'city', 'state', 'zip_code']
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    store = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'brand', 'category', 'model_year', 'list_price', 'store']
+
+    def get_store(self, obj):
+        # Get all StoreProduct entries related to the given product
+        store_products = StoreProduct.objects.filter(product=obj).select_related('store')
+
+        serialized_stores = []
+
+        for store_product in store_products:
+            store_data = StoreSerializer(store_product.store).data
+
+            serialized_stores.append({
+                'store': store_data
+            })
+
+        return serialized_stores
+
+
 @api_view(["POST", "PUT"])
 def add_product_to_brand(request, brand_id):
     Brand.objects.get(id=brand_id)
@@ -122,3 +152,24 @@ def delete_product_from_store(request, store_id, product_id):
     product.delete()
 
     return Response({'message': 'Products successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET"])
+def get_products_details(request):
+    store_id = request.GET.get('store_id')
+    category_id = request.GET.get('category_id')
+
+    products = Product.objects.all()
+
+    if store_id:
+        products = products.filter(storeproduct__store_id=store_id)
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
+    page_number = request.GET.get('page')
+    per_page = request.GET.get('page_size')
+
+    serializer = ProductDetailSerializer(custom_paginator(products, page_number, per_page), many=True)
+
+    return Response(serializer.data)
